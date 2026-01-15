9aedd19a2da27818e012a9b57795d8167ec098bc
watchyourback <104729221+worhllo@users.noreply.github.com>
2026-01-16 00:56:45 +0900

    Add short link: mylink

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -1242,5 +1242,9 @@ window.RULES_INTERMEDIATE = {
     "/ceshiw": {
         "url": "https://paste.142588.xyz/paste/QpBKuQ",
         "expired_at": "2026-01-15T15:26:00.000Z"
+    },
+    "/mylink": {
+        "url": "https://github.com/ginobefun/BestBlogs",
+        "expired_at": "2026-01-15T16:03:00.000Z"
     }
 };
